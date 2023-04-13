b1b421dbfb39ce755de7c73b419aeb5eb7e17c97
zaidkazi3008 <zaidkazi.in@gmail.com>
2023-04-13 11:16:34 +0530

    update again

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cookieParser = require('cookie-parser');
 // 1) MIDDLEWARES
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   })
