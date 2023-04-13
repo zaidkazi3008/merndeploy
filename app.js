1c901d13f968f8b8dd9b449a1bb6ddd9941fb01f
zaidkazi3008 <zaidkazi.in@gmail.com>
2023-04-13 11:10:41 +0530

    Revert "update app.js"
    This reverts commit 25b0b9ac0828e4b141725b509b289719320d4d50.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const cookieParser = require('cookie-parser');
 // 1) MIDDLEWARES
 app.use(
   cors({
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   })
